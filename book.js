f25412c4569805cb4aa49daa63bca5c33697be53
jhettchen <jesse.hettchen@gmail.com>
2026-09-06 07:07:40 -0700

    Add files via upload

--- book.js
+++ book.js
@@ -8,7 +8,6 @@ if (book) {
   document.getElementById("book-cover").alt = book.title;
   document.getElementById("book-title").textContent = book.title;
   document.getElementById("book-author").textContent = book.author;
-  document.getElementById("book-status").textContent = book.status === "active" ? "ACTIVE" : "ARCHIVE";
   document.getElementById("book-blurb").textContent = book.blurb;
 } else {
   document.querySelector("main").innerHTML = "<p>Book not found.</p>";
